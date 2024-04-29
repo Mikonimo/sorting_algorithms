b75c4f7087573a21358eaba5f67bae2aac43fb0b
Mikonimo <mikonani413@gmail.com>
2024-04-29 16:29:44 +0300

    sorting tasks

---
+++ 3-quick_sort.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order
+ * using the Quick sort algorithm (Lomuto partition scheme)
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quicksort_recursive(array, 0, size - 1, size);
+}
+
+/**
+ * quicksort_recursive - Recursively sorts an array of integer
+ * using the Quick sort algorithm
+ * @array: The array to be sorted
+ * @low: The starting index of the partition to be sorted
+ * @high: The ending index of the partition to be sorted
+ * @size: Number of elements in the array
+ */
+void quicksort_recursive(int *array, int low, int high, size_t size)
+{
+	int pivot_idx;
+
+	if (low < high)
+	{
+		pivot_idx = partition(array, low, high, size);
+		quicksort_recursive(array, low, pivot_idx - 1, size);
+		quicksort_recursive(array, pivot_idx + 1, high, size);
+	}
+}
+
+/**
+ * partition - Partitions an array around a pivot
+ * @array: The array to be partitioned
+ * @low: The starting index of the partition
+ * @high: The ending index of the partition
+ * @size: Number of elements in the array
+ *
+ * Return: The index of the pivot element after partitioning
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j, temp;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			print_array(array, size);
+		}
+	}
+	temp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp;
+	print_array(array, size);
+	return (i + 1);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Sorting algorithm is an algorithm that puts elements of a list into an order. The most frequently used orders are numerical order and lexicographical order, and either ascending or descending.
+This Repository is focused sorting algorithms.
